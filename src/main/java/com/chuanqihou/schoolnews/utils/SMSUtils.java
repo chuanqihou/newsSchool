4fa610d2e1860c89eb6455f906ca13830c558fa9
chuanqihou <chuanqihou@qq.com>
2022-11-30 15:57:02 +0800

    校园新闻平台

--- src/main/java/com/chuanqihou/schoolnews/utils/SMSUtils.java
+++ src/main/java/com/chuanqihou/schoolnews/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tL5VyoE2FSBHb3LxnkM", "rzpS8WRJtgjrsxAoBTAsv2ZNyihJgh");
+		DefaultProfile profile = DefaultProfile.getProfile("", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
